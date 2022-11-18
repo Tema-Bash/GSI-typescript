0ef32f1565087d2bbff2f2a19cc20e189727c03c
Bashakin <bashakin@greensight.ru>
2022-11-18 15:43:43 +0300

    fix 2 task

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
 /*
-Практика:
-1. Создать 2 интерфейса:
-InterfaceExampleOne состоит из полей, a - строка, b - число, c - строка или массив строк, d - любой тип
-InterfaceExampleTwo имеет все те же поля что и InterfaceExampleOne только нет d, а "а" - число.
-2. Создать enum'ы FieldTypes, FieldTheme, FieldSize (ключи придумай сам) и интерфейс FieldProps.
-FieldProps состоит из: type - принимает на вход значение енама FieldTypes, size и theme - принимает на вход значение ключа,
- например, если типизировать какой-нибудь компонент или класс, то на вход можно отдать строку равную одному из ключей енама.
-3. Cоздать 2 интерфейса PropertyObjectOne и PropertyObjectTwo имеющие одинаковые поля p1 и p2, но у PropertyObjectOne p1 и p2
-имеют тип InterfaceExampleOne, а у PropertyObjectTwo поля имеют тип InterfaceExampleTwo. InterfaceExampleOne и InterfaceExampleTwo брать из
-п.1. Задачу выполнить через дженерики.
+    Практика:
+    1. Создать 2 интерфейса:
+    InterfaceExampleOne состоит из полей, a - строка, b - число, c - строка или массив строк, d - любой тип
+    InterfaceExampleTwo имеет все те же поля что и InterfaceExampleOne только нет d, а "а" - число.
 */
-//1)
+
 interface IFirst {
     a: string;
     b: number;
@@ -24,44 +18,55 @@ interface ISecond extends Omit<IFirst, 'a'> {
     c: number[];
 }
 
-//2)
-interface IFieldProps {
-    type: FieldTypes;
-    size: FieldSize;
-    theme: FieldTheme;
-}
+/*
+    2. Создать enum'ы FieldTypes, FieldTheme, FieldSize (ключи придумай сам) и интерфейс FieldProps.
+    FieldProps состоит из: type - принимает на вход значение енама FieldTypes, size и theme - принимает на вход значение ключа,
+    например, если типизировать какой-нибудь компонент или класс, то на вход можно отдать строку равную одному из ключей енама.
+*/
 
+/*
+FieldProps состоит из: type - принимает на вход значение енама
+ FieldTypes, size и theme - принимает на вход значение ключа,
+    например, если типизировать какой-нибудь компонент или
+    класс, то на вход можно отдать строку равную одному из
+    ключей енама.
+
+*/
 enum FieldTypes {
-    car,
-    flight,
-    ship,
-    spaceship,
+    car = 'volvo',
+    flight = 'to mos',
+    ship = 'titanic',
+    spaceship = 'b12',
 }
 
 enum FieldTheme {
-    dark,
-    light,
-    grey,
-    blue,
+    dark = '000',
+    light = 'fff',
+    grey = 'ddd',
+    blue = '00f',
 }
 
 enum FieldSize {
-    small,
-    medium,
-    large,
+    small = 's',
+    medium = 'm',
+    large = 'l',
+}
+interface IFieldProps {
+    type: FieldTypes;
+    size: keyof typeof FieldSize;
+    theme: keyof typeof FieldTheme;
 }
 
 let tmp: IFieldProps = {
     type: FieldTypes.car,
-    size: FieldSize.small,
-    theme: FieldTheme.blue,
+    size: 'large',
+    theme: 'grey',
 };
 
-//3)
 /*
-3. Cоздать 2 интерфейса PropertyObjectOne и PropertyObjectTwo имеющие одинаковые поля p1 и p2, но у PropertyObjectOne p1 и p2
-имеют тип InterfaceExampleOne, а у PropertyObjectTwo поля имеют тип InterfaceExampleTwo.
-InterfaceExampleOne и InterfaceExampleTwo брать из п.1. Задачу выполнить через дженерики.
+    3. Cоздать 2 интерфейса PropertyObjectOne и PropertyObjectTwo имеющие одинаковые поля p1 и p2, но у PropertyObjectOne p1 и p2
+    имеют тип InterfaceExampleOne, а у PropertyObjectTwo поля имеют тип InterfaceExampleTwo.
+    InterfaceExampleOne и InterfaceExampleTwo брать из п.1. Задачу выполнить через дженерики.
 */
 
 interface Generic<Type> {
